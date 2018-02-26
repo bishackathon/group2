0e401de507de5d3fd33247e8e76b97d8f55eab81
sam <samclouston86842@gmail.com>
2018-02-26 11:13:54 +0530

    test

--- hackathon project/Form1.vb
+++ hackathon project/Form1.vb
@@ -67,7 +67,7 @@
                 If TextBox5.Text = "" Then
                     TextBox5.Text = "Injustice 2"
                 Else
-                    TextBox6.Text = "Injustice 2"
+                    TextBox6.Text = "Injustice 2k"
                 End If
             End If
         Else
